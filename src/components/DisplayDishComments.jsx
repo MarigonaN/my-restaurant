9337d9d588f314a2da352b97ecbbbb9fd2efac21
MarigonaN <marigonanuhija@gmail.com>
2020-06-11 15:30:59 +0200

    Update DisplayDishComments.jsx

--- src/components/DisplayDishComments.jsx
+++ src/components/DisplayDishComments.jsx
@@ -1,45 +1,47 @@
 import React, { Component } from "react";
 import { Container, Alert, ListGroup, ListGroupItem, Badge } from "reactstrap";
 
-function DisplayDishComments(props) {
-  return (
-    <Container className="my-5">
-      {props.selectedDish ? (
-        <ListGroup>
-          {props.selectedDish.comments.map((comment, index) => {
-            var color = "";
-            switch (comment.rating) {
-              case 1:
-                color = "danger";
-                break;
-              case 2:
-                color = "warning";
-                break;
-              case 3:
-                color = "secondary";
-                break;
-              default:
-                color = "success";
-                break;
-            }
+class DisplayDishComments extends Component {
+  state = {};
+  render() {
+    return (
+      <Container className="my-5">
+        {this.props.selectedDish && (
+          <ListGroup>
+            {this.props.selectedDish.comments.map((comment, index) => {
+              var color = "";
+              switch (comment.rating) {
+                case 1:
+                  color = "danger";
+                  break;
+                case 2:
+                  color = "warning";
+                  break;
+                case 3:
+                  color = "secondary";
+                  break;
+                default:
+                  color = "success";
+                  break;
+              }
 
-            return (
-              <ListGroupItem key={index}>
-                {comment.author}: {comment.comment} |{" "}
-                <Badge pill color={color}>
-                  {comment.rating}
-                </Badge>
-              </ListGroupItem>
-            );
-          })}
-        </ListGroup>
-      ) : (
-        <Alert color="secondary">
-          No dish selected, please click on a Dish to show the comments
-        </Alert>
-      )}
-    </Container>
-  );
+              return (
+                <ListGroupItem key={index}>
+                  {comment.author}: {comment.comment} |{" "}
+                  <Badge pill color={color}>
+                    {comment.rating}
+                  </Badge>
+                </ListGroupItem>
+              );
+            })}
+          </ListGroup>
+        )}
+        {!this.props.selectedDish && (
+          <Alert color="secondary">No dish selected, please click on a Dish to show the comments</Alert>
+        )}
+      </Container>
+    );
+  }
 }
 
 export default DisplayDishComments;
